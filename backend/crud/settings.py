4d3947ebfab050fad229c6095bac685ec2ddc52b
abhinaya3018-hue <abhinaya3018@gmail.com>
2026-01-09 20:24:12 +0530

    create frontend

--- backend/crud/settings.py
+++ backend/crud/settings.py
@@ -38,11 +38,17 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'api',
+    'corsheaders',
+]
+
+CORS_ALLOWED_ORIGINS = [
+   'http://localhost:5173',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',  # ✅ Must be at the very top
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
